6c13154c831b738566335f59cc5e6abdc159e170
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-04-15 23:31:42 +0200

    prepare urls for production deployment

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -1,39 +1,38 @@
 const GUEST_LOGINS = {
-    customer : {
-        username: 'andrey',
-        password: 'asdasd'
-    },
-    business : {
-        username: 'kevin',
-        password: 'asdasd24'
-    }
-}
+  customer: {
+    username: "Andrey",
+    password: "andrey1234",
+  },
+  business: {
+    username: "Kevin",
+    password: "kevin1234",
+  },
+};
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-const STATIC_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = "https://api.coderr.saschaheinze.de/api/";
+const STATIC_BASE_URL = "https://api.coderr.saschaheinze.de/";
 
+const LOGIN_URL = "login/";
 
-const LOGIN_URL = 'login/';
+const REGISTER_URL = "registration/";
 
-const REGISTER_URL = 'registration/';
+const PROFILE_URL = "profile/";
 
-const PROFILE_URL = 'profile/';
+const BUSINESS_PROFILES_URL = "profiles/business/";
 
-const BUSINESS_PROFILES_URL = 'profiles/business/';
+const CUSTOMER_PROFILES_URL = "profiles/customer/";
 
-const CUSTOMER_PROFILES_URL = 'profiles/customer/';
+const REVIEW_URL = "reviews/";
 
-const REVIEW_URL = 'reviews/';
+const ORDER_URL = "orders/";
 
-const ORDER_URL = 'orders/';
+const OFFER_URL = "offers/";
 
-const OFFER_URL = 'offers/';
+const OFFER_DETAIL_URL = "offerdetails/";
 
-const OFFER_DETAIL_URL = 'offerdetails/';
+const BASE_INFO_URL = "base-info/";
 
-const BASE_INFO_URL = 'base-info/';
+const OFFER_INPROGRESS_COUNT_URL = "order-count/";
+const OFFER_COMPLETED_COUNT_URL = "completed-order-count/";
 
-const OFFER_INPROGRESS_COUNT_URL = 'order-count/';
-const OFFER_COMPLETED_COUNT_URL = 'completed-order-count/';
-
-const PAGE_SIZE = 6
+const PAGE_SIZE = 6;
